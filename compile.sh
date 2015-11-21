9f32bcb26932fc0787a32f54338c4a5a7befade2
Tobia Zorzan <tobia.zorzan@gmail.com>
2015-11-21 20:01:03 +0100

    Working on cap 8.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 pdflatex -output-directory=output tesi.tex
-biber --input-directory output/ --output-directory output/ tesi
+biber --output-directory output/ tesi
 pdflatex -output-directory=output tesi.tex
 cp output/tesi.pdf .
